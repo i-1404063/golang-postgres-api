4a36f1ce42c063c98d7af637179c5e2b5b307832
Mahedi Hasan <mhasan@cleversow.com>
2021-04-08 18:51:11 +0600

    two get and one post request handler implemented

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
+	"strings"
+	"sync"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -11,6 +16,7 @@ import (
 )
 
 type Person struct {
+	sync.Mutex
 	gorm.Model
 
 	Name  string
@@ -29,7 +35,7 @@ type Book struct {
 
 var (
 	person = &Person{Name: "imon", Email: "imonhasans33@gmail.com"}
-	books = []Book{
+	books  = []Book{
 		{Title: "Captive Lady", Author: "Michael Madhusudhon Dutta", CallNumber: 25, PersonID: 1},
 		{Title: "Hamlet", Author: "Shakespear", CallNumber: 20, PersonID: 2},
 	}
@@ -50,12 +56,12 @@ func goDotEnv(key string) string {
 	return os.Getenv(key)
 }
 
-//Environment variables
+// Environment variables
 var (
-	host = goDotEnv("HOST")
-	dbPort = goDotEnv("DBPORT")
-	user = goDotEnv("USER")
-	dbName = goDotEnv("NAME")
+	host     = goDotEnv("HOST")
+	dbPort   = goDotEnv("DBPORT")
+	user     = goDotEnv("USER")
+	dbName   = goDotEnv("NAME")
 	password = goDotEnv("PASSWORD")
 )
 
@@ -71,6 +77,9 @@ func main() {
 		fmt.Println("Successfully connected to the database!")
 	}
 
+	// close connection when main function finishes
+	// defer db.Close()
+
 	// Make migrations to database
 	db.AutoMigrate(&Person{})
 	db.AutoMigrate(&Book{})
@@ -80,4 +89,78 @@ func main() {
 	// for idx:= range books {
 	// 	db.Create(&books[idx])
 	// }
+
+	// handler route
+	http.HandleFunc("/lib/users", getPeople)
+	http.HandleFunc("/lib/user/", getPerson)
+	http.HandleFunc("/lib/users/create", createPerson)
+	http.ListenAndServe(":3000", nil)
+}
+
+// all handler implemented here
+func createPerson(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			w.Write([]byte("Provide proper information"))
+			return
+		}
+
+		var person Person
+		ct := r.Header.Get("content-type")
+		if ct != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
+			w.Header().Set("need content-type 'application/json' but got %s", ct)
+			w.Write([]byte("Only Allowed Content-Type 'application/json'"))
+			return
+		}
+
+		json.Unmarshal(bodyBytes, &person)
+
+		createdPerson := db.Create(&person)
+		err = createdPerson.Error
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Something wrong happened"))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(&person)
+
+	} else {
+		w.Header().Set("Allowed method", http.MethodPost)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Only post request is valid for this route"))
+	}
+}
+
+func getPerson(w http.ResponseWriter, r *http.Request) {
+	path := strings.Split(r.URL.String(), "/")
+	if len(path) != 4 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Id is not provided"))
+		return
+	}
+
+	var person Person
+	var books []Book
+	// db.Find(&person, "id = ?", path[3])
+	db.First(&person, path[3])
+	db.Preload("person").Find(&books, "person_id = ?", path[3]) // it will give all the books
+	// db.Preload("person").First(&books, path[3]) // it will give only one book
+
+	person.Books = books
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&person)
+}
+
+func getPeople(w http.ResponseWriter, r *http.Request) {
+	var person []Person
+
+	db.Find(&person)
+	json.NewEncoder(w).Encode(&person)
 }
